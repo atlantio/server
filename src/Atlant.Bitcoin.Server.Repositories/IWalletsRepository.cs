722c6cffb5faa704728774ee76b13597ffff768e
MobyDi21 <dimasheludko.work@gmail.com>
2019-03-29 12:41:41 -0700

    add repos proj and wallets repo

---
+++ src/Atlant.Bitcoin.Server.Repositories/IWalletsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Atlant.Bitcoin.Server.Domain.Entities;
+
+namespace Atlant.Bitcoin.Server.Repositories
+{
+    public interface IWalletsRepository
+    {
+        Task<IReadOnlyCollection<Wallet>> GetHotWalletsAsync();
+    }
+}
