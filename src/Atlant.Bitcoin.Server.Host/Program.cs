5b6dd037867b9ac12cb24a81eee28ac64242ffc2
MobyDi21 <dimasheludko.work@gmail.com>
2019-03-29 09:32:26 -0700

    add blank host project

---
+++ src/Atlant.Bitcoin.Server.Host/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Atlant.Bitcoin.Server.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (var host = CreateWebHost(args))
+                    host.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Environment.Exit(-1);
+            }
+        }
+
+        public static IWebHost CreateWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
